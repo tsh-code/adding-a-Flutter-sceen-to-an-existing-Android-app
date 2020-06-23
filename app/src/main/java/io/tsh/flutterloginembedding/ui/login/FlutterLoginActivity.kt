41089b3e70e5fb97d690c4997b6ebede7bffd69e
Damian Zawadzki <damian.zawadzki@thesoftwarehouse.pl>
2020-06-23 10:36:22 +0200

    Navigation from LoginActivity.kt to FlutterLoginActivity.kt, Including new_login_screen.dart in main.dart.

--- app/src/main/java/io/tsh/flutterloginembedding/ui/login/FlutterLoginActivity.kt
+++ app/src/main/java/io/tsh/flutterloginembedding/ui/login/FlutterLoginActivity.kt
@@ -4,45 +4,15 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class FlutterLoginActivity : AppCompatActivity() {
+class FlutterLoginActivity : FlutterFragmentActivity() {
 
-    private lateinit var loginViewModel: LoginViewModel
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        /* poof */
-        loginViewModel = ViewModelProviders.of(this, LoginViewModelFactory())
-            .get(LoginViewModel::class.java)
-
-        loginViewModel.loginFormState.observe(this@FlutterLoginActivity, Observer {
-            val loginState = it ?: return@Observer
-            /* poof */ loginState.isDataValid
-
-            if (loginState.usernameError != null) {
-                /* poof */
-            }
-            if (loginState.passwordError != null) {
-                /* poof */
-            }
-        })
-
-        loginViewModel.loginResult.observe(this@FlutterLoginActivity, Observer {
-            val loginResult = it ?: return@Observer
-            /* poof */
-            if (loginResult.error != null) {
-                /* poof */
-            }
-            if (loginResult.success != null) {
-                /* poof */
-            }
-            /* poof */
-        })
-
-        /* poof */
-        loginViewModel.loginDataChanged("poof user", "poof password")
-        /* poof */
-        loginViewModel.login("poof user", "poof password")
-        /* poof */
+    companion object {
+        fun withNewEngine() = CustomNewEngineIntentBuilder()
     }
+
+    class CustomNewEngineIntentBuilder :
+        FlutterFragmentActivity.NewEngineIntentBuilder(FlutterLoginActivity::class.java)
+    
 }
--- app/src/main/java/io/tsh/flutterloginembedding/ui/login/LoginActivity.kt
+++ app/src/main/java/io/tsh/flutterloginembedding/ui/login/LoginActivity.kt
@@ -16,6 +16,7 @@ import android.widget.ProgressBar
 import android.widget.Toast
 
 import io.tsh.flutterloginembedding.R
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
@@ -32,7 +33,7 @@ class LoginActivity : AppCompatActivity() {
         val loading = findViewById<ProgressBar>(R.id.loading)
 
         loginViewModel = ViewModelProviders.of(this, LoginViewModelFactory())
-                .get(LoginViewModel::class.java)
+            .get(LoginViewModel::class.java)
 
         loginViewModel.loginFormState.observe(this@LoginActivity, Observer {
             val loginState = it ?: return@Observer
@@ -66,16 +67,16 @@ class LoginActivity : AppCompatActivity() {
 
         username.afterTextChanged {
             loginViewModel.loginDataChanged(
-                    username.text.toString(),
-                    password.text.toString()
+                username.text.toString(),
+                password.text.toString()
             )
         }
 
         password.apply {
             afterTextChanged {
                 loginViewModel.loginDataChanged(
-                        username.text.toString(),
-                        password.text.toString()
+                    username.text.toString(),
+                    password.text.toString()
                 )
             }
 
@@ -83,8 +84,8 @@ class LoginActivity : AppCompatActivity() {
                 when (actionId) {
                     EditorInfo.IME_ACTION_DONE ->
                         loginViewModel.login(
-                                username.text.toString(),
-                                password.text.toString()
+                            username.text.toString(),
+                            password.text.toString()
                         )
                 }
                 false
@@ -94,6 +95,13 @@ class LoginActivity : AppCompatActivity() {
                 loading.visibility = View.VISIBLE
                 loginViewModel.login(username.text.toString(), password.text.toString())
             }
+
+            newLogin.setOnClickListener {
+                val intent = FlutterLoginActivity
+                    .withNewEngine()
+                    .build(this@LoginActivity)
+                startActivity(intent)
+            }
         }
     }
 
@@ -102,9 +110,9 @@ class LoginActivity : AppCompatActivity() {
         val displayName = model.displayName
         // TODO : initiate successful logged in experience
         Toast.makeText(
-                applicationContext,
-                "$welcome $displayName",
-                Toast.LENGTH_LONG
+            applicationContext,
+            "$welcome $displayName",
+            Toast.LENGTH_LONG
         ).show()
     }
 
