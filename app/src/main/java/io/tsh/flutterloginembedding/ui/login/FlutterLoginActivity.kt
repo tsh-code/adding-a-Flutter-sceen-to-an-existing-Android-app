26c8a10c8399ab11c57334f68e94058fb57d5f7e
Damian Zawadzki <damian.zawadzki@thesoftwarehouse.pl>
2020-06-24 21:49:25 +0200

    Splash screen with white background for transition between native and flutter screen.

--- app/src/main/java/io/tsh/flutterloginembedding/ui/login/FlutterLoginActivity.kt
+++ app/src/main/java/io/tsh/flutterloginembedding/ui/login/FlutterLoginActivity.kt
@@ -1,10 +1,16 @@
 package io.tsh.flutterloginembedding.ui.login
 
+import android.content.Context
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.ImageView
+import androidx.core.content.ContextCompat
+import io.flutter.embedding.android.DrawableSplashScreen
 import io.flutter.embedding.android.FlutterFragmentActivity
+import io.flutter.embedding.android.SplashScreen
+import io.tsh.flutterloginembedding.R
 
 class FlutterLoginActivity : FlutterFragmentActivity() {
 
@@ -14,5 +20,12 @@ class FlutterLoginActivity : FlutterFragmentActivity() {
 
     class CustomNewEngineIntentBuilder :
         FlutterFragmentActivity.NewEngineIntentBuilder(FlutterLoginActivity::class.java)
-    
+
+    override fun provideSplashScreen(): SplashScreen =
+        DrawableSplashScreen(
+            ColorDrawable(ContextCompat.getColor(this, R.color.white)),
+            ImageView.ScaleType.CENTER,
+            0
+        )
+
 }
